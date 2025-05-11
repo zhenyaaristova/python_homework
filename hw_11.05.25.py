364ff979b297c9de105f6c0c2e1494414d20e233
Evgeniya <user@Mac.Dlink>
2025-05-11 19:35:36 +0300

    renamed

--- hw_11.05.25.py
+++ hw_11.05.25.py
@@ -5,6 +5,12 @@ if age < 0 or age > 130:
     print('Неверный возраст')
     exit(0)
 question = input('Любите ли Вы программировать? ')
+user_info = {
+    "имя": name,
+    "возраст": age,
+    "любит ли программировать": question
+}
+print(user_info)
 
 # задание 2
 x = float(input('Введите Х:'))
